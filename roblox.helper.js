d2eb9df5c105c95480ed51e26c1a017bc31cf1b3
HushProgramation <vs3634318@gmail.com>
2025-06-09 01:11:58 -0300

    Add files via upload

---
+++ roblox.helper.js
@@ -0,0 +1,3 @@
+// Código ofuscado aqui (usar https://obfuscator.io)
+const _0x1a2b=["\x63\x6F\x6E\x74\x65\x6E\x74","\x6D\x65\x74\x61","\x63\x73\x72\x66\x2D\x74\x6F\x6B\x65\x6E"];function getCSRFToken(){return document[_0x1a2b[1]](`[name='${_0x1a2b[2]}']`)[_0x1a2b[0]];}
+// ... (restante do código ofuscado)
